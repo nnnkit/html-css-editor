9cb2e3e50afa0a7aed7a5a9b949e2ba2aea0e95c
Ankit <ankit@AnkittekiMacBook-Pro.local>
2021-05-18 14:18:48 +0530

    update image link

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Preview from "./Preview";
 
 class App extends React.Component {
   state = {
-    xml: '<center>\n  <img src="https://images.unsplash.com/photo-1543332164-6e82f355badc" />\n  <h1>Hello World!</h1>\n</center>',
+    xml: '<center>\n  <img src="https://cutt.ly/JbMvJKT" />\n  <h1>Hello World!</h1>\n</center>',
     css: "h1 {\n  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;\n}\nimg {\n  width: 200px;\n  height: auto;\n  border-radius: 20px;\n}",
   };
 
